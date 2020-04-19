27bdccfb675c328229cb22cbacc6c3d6a6b2cbbc
Meng <sunmeng90@126.com>
2020-04-20 06:32:30 +0800

    fix lifetime error in list10-16

--- examples/list10-16/src/main.rs
+++ examples/list10-16/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 fn main() {
     use std::fmt::Display;
 
@@ -7,12 +8,13 @@ fn main() {
     }
 
     impl<T> Pair<T> {
-        fn new(x: T, y: T) -> &Self {
+        fn new(x: T, y: T) -> Self {
             Self { x: x, y: y }
         }
     }
     // Pair<T> only implements the cmp_display method if its inner type T implements the PartialOrd trait that enables comparison and the Display trait that enables printing.
-    impl<T: Display + PartialOrd> Pair<T> { // Conditionally implement methods on a generic type depending on trait bounds
+    impl<T: Display + PartialOrd> Pair<T> {
+        // Conditionally implement methods on a generic type depending on trait bounds
         fn cmp_display(&self) {
             if self.x >= self.y {
                 println!("The largest member is x = {}", self.x);
